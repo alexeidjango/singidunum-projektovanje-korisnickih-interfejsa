bdd1bc3431293c296bed7c99f95ad4684bc3dfcc
Alexei Melnikov <alexeidjango@gmail.com>
2026-08-29 18:27:53 +0200

    add main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Catalog } from './catalog/catalog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Katalog igračaka', component: Catalog },
+  { path: '**', redirectTo: '' },
+];
